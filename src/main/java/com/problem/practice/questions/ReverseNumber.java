c01bbe0ce75f8667c543ee2638eafc07904af654
PraveenJJ <praveen_jj123@yahoo.com>
2025-11-26 22:30:29 +0530

    Problem 1 - How to reverse a number

---
+++ src/main/java/com/problem/practice/questions/ReverseNumber.java
@@ -0,0 +1,36 @@
+package com.problem.practice.questions;
+
+import java.util.Scanner;
+
+/**
+ * To reverse a number
+ * divide number by 10
+ * remainder will give last digit
+ * concatenate to an empty string
+ * quotient will give number excluding last digit
+ * use the quotient as new number & repeat until quotient is finally 0
+ * Problem: https://prepinsta.com/interview-preparation/technical-interview-questions/most-asked-coding-questions-in-placements/
+ */
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int inputNumber = scanner.nextInt();
+        String reveredString = "";
+
+        while (inputNumber != 0) {
+            // remainder will be last digit
+            int lastDigit = inputNumber % 10;
+
+            // concatenate lastDigit
+            reveredString = reveredString + lastDigit;
+
+            // quotient excludes last digit
+            inputNumber = inputNumber / 10;
+        }
+
+        System.out.println("Reversed number: " + reveredString);
+    }
+
+}
